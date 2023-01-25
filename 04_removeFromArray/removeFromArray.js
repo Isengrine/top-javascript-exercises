fb46857411934b0b4872d36b584159eac647ff40
Isengrine <isengrine@gmail.com>
2023-01-25 17:46:26 -0600

    Add exercise 4 files

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...items) {
+    for (let item of items) {
+        if (array.includes(item)) {
+            let index = array.indexOf(item);
+            const splitArray = array.splice(index, 1);
+        }
 
+        else {
+            continue
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
