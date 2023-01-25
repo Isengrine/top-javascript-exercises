9910f23f587d329df8d508092bf31bd7cf7a0a93
Isengrine <isengrine@gmail.com>
2023-01-25 05:45:56 -0600

    Add exercise 3 files

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+function reverseString(str) {
 
+    let chars = str.split("");
+    let revStr = "";
+    
+    for (let i = 0; i < str.length; i++) {
+        revStr += chars.pop()
+    }
+
+    return revStr;
+    
 };
 
 // Do not edit below this line
