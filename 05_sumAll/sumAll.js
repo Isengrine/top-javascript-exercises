f9a852dd1021c5aee3bc739a64db4881bc2feff2
Isengrine <isengrine@gmail.com>
2023-01-25 19:32:25 -0600

    Add exercise 5 files

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+function sumAll(a, b) {
+
+    let sum = 0
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    }
+
+    else if (a > b) {
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+    }
+
+    else {
+        return "ERROR";
+    }
+
+    return sum;
 
 };
 
